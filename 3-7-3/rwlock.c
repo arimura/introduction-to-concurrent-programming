7a689c583a33ad5a183527d645100e12c4d7f26f
arimura <kotaro_arimura@voyagegroup.com>
2022-02-28 16:10:04 +0900

    Add

---
+++ 3-7-3/rwlock.c
@@ -0,0 +1,11 @@
+#include <pthread.h>
+#include <stdlib.h>
+
+pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
+void do_lock(){
+    pthread_rwlock_rdlock(&lock);
+    for(uint64_t i =0; i < HOLDTIME; i++){
+        asm volatile("nop");
+    }
+    pthread_rwlock_unlock(&lock);
+}
--- makefile
+++ makefile
@@ -2,4 +2,4 @@ kick-rust/%:
 	rustc $*/main.rs && ./main
 
 kick-c/%:
-	clang $*/main.c && ./a.out
+	clang $*/*.c && ./a.out
