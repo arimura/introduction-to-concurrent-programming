fd3a0a4601ef5b8ec6436de6132ddcd881234530
arimura <kotaro_arimura@voyagegroup.com>
2022-04-17 11:06:37 +0900

    Add demos

---
+++ rust/arc/main.rs
@@ -0,0 +1,14 @@
+use std::sync::Arc;
+use std::thread;
+
+fn main(){
+    let apple = Arc::new("the same apple");
+
+    for _ in 0..10 {
+        let apple = Arc::clone(&apple);
+
+        thread::spawn(move || {
+            println!("{:?}", apple);
+        });
+    }
+}
